c437965d3f8be4a85cdf09a278d56a5bbf4cdfab
Vlastimil Dejl <vlastimil.dejl@artatol.com>
2015-02-11 06:36:58 +0100

    adwords remarketing policy

---
+++ app/presenters/PolicyPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+
+/**
+ * Policy presenter.
+ */
+class PolicyPresenter extends BasePresenter {
+
+    public function renderDefault() {
+
+    }
+
+}
