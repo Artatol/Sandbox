24f822fcc4f29659f067b3f4b9f26a76bf591e22
Vlastimil Dejl <vlastimil.dejl@artatol.com>
2015-02-16 10:13:18 +0100

    pdf success

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -15,9 +15,8 @@ class HomepagePresenter extends BasePresenter {
 	}
 
 	public function renderPdf() {
-		require_once('../vendor/tecnick.com/tcpdf/tcpdf.php');
 		// create new PDF document
-		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 		$pdf->SetCreator(PDF_CREATOR);
