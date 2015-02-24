87511c4f273d7a17999a3f02afd435f3bca5c47c
Vlastimil Dejl <vlastimil.dejl@artatol.com>
2015-02-24 17:37:32 +0100

    update

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -14,16 +14,43 @@ class HomepagePresenter extends BasePresenter {
 
 	}
 
+	class MYPDF extends TCPDF {
+		//Page header
+		public function Header()
+		{
+			// Logo
+			$image_file = K_PATH_IMAGES . 'logo_example.jpg';
+			$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+			// Set font
+			$this->SetFont('helvetica', 'B', 20);
+			// Title
+			$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+		}
+
+		// Page footer
+		public function Footer()
+		{
+			// Position at 15 mm from bottom
+			$this->SetY(-15);
+			// Set font
+			$this->SetFont('helvetica', 'I', 8);
+			// Page number
+			$this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+		}
+	}
+
 	public function renderPdf() {
 		// create new PDF document
-		$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$pdf = new \MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 		$pdf->SetCreator(PDF_CREATOR);
-		$pdf->SetAuthor('Nicola Asuni');
-		$pdf->SetTitle('TCPDF Example 001');
-		$pdf->SetSubject('TCPDF Tutorial');
-		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+		$pdf->SetAuthor('SMC group');
+		$pdf->SetTitle('Model agreement');
+		$pdf->SetSubject('Model agreement');
+		$pdf->SetKeywords('SMC group, PDF, agreement');
+
+
 
 // set default header data
 		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
@@ -87,6 +114,6 @@ EOD;
 
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
-		$pdf->Output('example_001.pdf', 'I');
+		$pdf->Output('agreement_001.pdf', 'I');
 	}
 }
